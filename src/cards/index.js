c28cf56241b184181ade719c6111fcb0bda7e18b
SanskritiSaluja <sanskriti.saluja@tothenew.com>
2021-07-08 20:10:43 +0530

    Added sticky nav

--- src/cards/index.js
+++ src/cards/index.js
@@ -17,9 +17,10 @@ import './cards.scss'
 const Cards = (props) => {
     const [allCards,setAllCards] = useState([])
     const [offset,setOffSet] = useState(8)
+    const [stickyNav , setStickyNav] = useState(false)
 
     useEffect (() => {
-        window.addEventListener('scroll', infiniteScroll);
+        window.addEventListener('scroll', listenScrollEvent);
     })
 
     useEffect(() => {
@@ -32,34 +33,41 @@ const Cards = (props) => {
 
     },[offset])
 
-    const infiniteScroll = () => {
+    const navListLeft = ["Trending" , "Videos" , "Stories", "Quizzes" , "Memes","Spotlight"] 
+    const navListRight = [ <DownOutlined />, <UserOutlined/>,<SearchOutlined/>,<MenuOutlined/>] 
+
+    const listenScrollEvent = e => {
+        if (window.scrollY > 170) {
+          setStickyNav(true)
+        } else {
+          setStickyNav( false)
+        }
         if (window.innerHeight + document.documentElement.scrollTop-23 == document.documentElement.offsetHeight){
             let pageOffSet = offset + 8 
              setOffSet(pageOffSet)
            }
-        }
-    const navListLeft = ["Trending" , "Videos" , "Stories", "Quizzes" , "Memes","Spotlight"] 
-    const navListRight = [ <DownOutlined />, <UserOutlined/>,<SearchOutlined/>,<MenuOutlined/>] 
-  
+      }
+    
+     
     return (
         <div>                     
         <div className = "banner">
-            <header>
+            <header className = {stickyNav ? "sticky" : ""}>
                 <div className = "row">
                     <div className = "col-1">
-                    <img src = {NavIcon}/>
+                    <img src = {stickyNav ? StickyNaviIcon : NavIcon}/>
                     </div>
                     <div className = "col-8">
                       <div className = "navlist-left">
                     {navListLeft.map(ele => {
-                            return  <div className="nav-item">
+                            return  <div className=  {stickyNav ? "nav-item nav-item-sticky " : "nav-item"}>
                             {ele}
                         </div>
                         })}
                      </div>
                     </div>
                     <div className = "col-3">
-                    <div className = "navlist-left">
+                    <div className = "navlist-left" >
                     {navListRight.map(ele => {
                             return ele
                         })}
@@ -78,10 +86,7 @@ const Cards = (props) => {
 
 
       </div>
-   
-   
-   
-      <div className = "cards"> 
+       <div className = "cards"> 
         <div className = "row">
             {allCards?.map(item => {
                 return (
